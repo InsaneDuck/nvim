e9f8245e2308b0c44c7858013ae1adab0451da50
InsaneDuck <>
2025-02-21 01:46:54 +0530

    Updated Dashboard

---
+++ lua/plugins/dashboard.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "snacks.nvim",
+        opts = {
+            dashboard = {
+                preset = {
+                    pick = function(cmd, opts)
+                        return LazyVim.pick(cmd, opts)()
+                    end,
+                    header = [[ Neovim ]],
+                    -- stylua: ignore
+                    ---@type snacks.dashboard.Item[]
+                    keys = {
+                        { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                        { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                        { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                        { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
+                        { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                        { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                        { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
+                        { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+                        { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                    },
+                },
+                sections = {
+                    { section = "header" },
+                    { section = "keys", gap = 1, padding = 1 },
+                    { pane = 2, section = "terminal", cmd = "fortune | cowsay; sleep .1", hl = "header", padding = 1, indent = 8 },
+                    { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = { 1, 1 } },
+                    { section = "startup" },
+                },
+            },
+        },
+    }
+}
